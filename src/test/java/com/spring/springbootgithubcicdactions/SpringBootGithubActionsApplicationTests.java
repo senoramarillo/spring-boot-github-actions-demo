e70b8b88416cb3539b9ea83df285db9af0a061f7
Anonymous <anonymous@test.com>
2023-05-23 22:48:27 +0200

    initial commit

---
+++ src/test/java/com/spring/springbootgithubcicdactions/SpringBootGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootgithubcicdactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
